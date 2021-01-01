a4e31ba858e795c607b0d6ee32e2f88930dbcf56
mig-moog <orangej0312@gmail.com>
2021-01-01 14:11:28 -0500

    fixed modules

--- source/Main.hx
+++ source/Main.hx
@@ -2,11 +2,10 @@ package;
 
 import flixel.FlxGame;
 import openfl.display.Sprite;
+import states.PlayState;
 
-class Main extends Sprite
-{
-	public function new()
-	{
+class Main extends Sprite {
+	public function new() {
 		super();
 		addChild(new FlxGame(0, 0, PlayState));
 	}
--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -1,16 +1,13 @@
-package;
+package states;
 
 import flixel.FlxState;
 
-class PlayState extends FlxState
-{
-	override public function create()
-	{
+class PlayState extends FlxState {
+	override public function create() {
 		super.create();
 	}
 
-	override public function update(elapsed:Float)
-	{
+	override public function update(elapsed:Float) {
 		super.update(elapsed);
 	}
 }
